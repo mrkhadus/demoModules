85e0044681e7a83352b9f6418bf6e8f13bf6b3d4
Hrishikesh Dherange <hrishikeshdherange@gmail.com>
2025-01-31 20:23:08 +0530

    Test modules

--- main.tf
+++ main.tf
@@ -4,12 +4,24 @@ variable "source_vault_name" {
   type = string
 }
 
+variable "create_backup_vault" {
+  type    = bool
+  default = true
+}
+
+variable "create_backup_vault_policy" {
+  type    = bool
+  default = true
+}
+
 resource "aws_backup_vault" "source_vault" {
   name = var.source_vault_name
+  count = var.create_backup_vault ? 1 : 0
 }
 
 resource "aws_backup_vault_policy" "demo" {
   backup_vault_name = aws_backup_vault.source_vault.name
+  count = var.create_backup_vault_policy ? 1 : 0
   policy = jsonencode({
     "Statement": [
       {
